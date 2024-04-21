453015c5dd2539fcdfc89a89c0e980f61dcc0fde
vernetto <Ernesto0z>
2024-04-21 12:27:44 +0200

    add all

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.sftp.server.SftpSubsystemFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Collections;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+	public void startServer() throws IOException {
+
+		SshServer server = SshServer.setUpDefaultServer();
+		server.setPort(22);
+		server.setPasswordAuthenticator((username, password, session) -> true); // For demo purposes
+		server.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(Paths.get("hostkey.ser")));
+		server.setSubsystemFactories(Collections.singletonList(new SftpSubsystemFactory()));
+		server.start();
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		startServer();
+	}
+}
